abb46b8e0d3b33a20a60ca6d8a7c8a635cf88863
pzel <p@manisola.net>
2013-04-16 13:05:07 +0200

    remove redundancy from Behavior datatype

--- frp.hs
+++ frp.hs
@@ -26,23 +26,20 @@ instance Ord Time where
 {- dropping typeclasses, as shown at: http://www.haskellforall.com/2012/05/scrap-your-type-classes.html -}
 
 data Behavior a = Behavior {
-  at :: (Behavior a) -> Time -> a
+  at :: Time -> a
 } 
 
-at' :: Behavior a -> Time -> a
-at' b = (at b) b               -- This is just syntax sugar
-
 time :: Behavior Time 
-time = Behavior { at = const id }
+time = Behavior { at = id }
 
 lift0 :: a -> Behavior a
-lift0 v = Behavior { at = const . const $ v }
+lift0 v = Behavior { at = const v }
 
 lift1 :: (a -> b) -> Behavior a -> Behavior b
-lift1 f ba@Behavior{..} = Behavior { at = \b t-> f (at ba t) }
+lift1 f ba = Behavior { at = \t -> f (at ba t) }
 
 lift2 :: (a -> b -> c) -> Behavior a -> Behavior b -> Behavior c
-lift2 f ba bb = Behavior { at = \b t -> f (at' ba t) (at' bb t) }
+lift2 f ba bb = Behavior { at = \t -> f (at ba t) (at bb t) }
 
 {- Test-related code & properties -}
 instance Arbitrary TimeValue where
@@ -56,22 +53,22 @@ prop_time_bottoms = \tvx tvy -> isBottom         (AtLeast tvx <= At tvy) &&
 		    	     	isBottom         (AtLeast tvx <= AtLeast tvy) &&
 				(not . isBottom) (At tvx <= At tvy)
 
-prop_timebehavior_id0 = \tv0 -> (at time) time (At tv0) == (At tv0)
-prop_timebehavior_id1 = \tv0 -> (at time) time (AtLeast tv0) == (AtLeast tv0)
+prop_timebehavior_id0 = \tv0 -> at time (At tv0) == (At tv0)
+prop_timebehavior_id1 = \tv0 -> at time (AtLeast tv0) == (AtLeast tv0)
 
-prop_lift0 v = \t -> at' (lift0 v) t == v 
+prop_lift0 v = \t -> at (lift0 v) t == v 
   where types = (v :: Int)
 
 prop_lift1_plus c v = \t-> let f = (+ c)
-	                    in at' (lift1 f (lift0 v)) t == f (at' (lift0 v) t)
+	                    in at (lift1 f (lift0 v)) t == f (at (lift0 v) t)
   where types = (c::Int,v::Int)
 
 prop_lift1_tupl c v = \t-> let f x = (c,x)
-	                    in at' (lift1 f (lift0 v)) t == f (at' (lift0 v) t)
+	                    in at (lift1 f (lift0 v)) t == f (at (lift0 v) t)
   where types = (c::Int,v::Int)
 
 prop_lift2_tupl v1 v2 = \t-> let f = (,)
-	                       in at' (lift2 f (lift0 v1) (lift0 v2)) t == f (at' (lift0 v1) t) (at' (lift0 v2) t)
+	                       in at (lift2 f (lift0 v1) (lift0 v2)) t == f (at (lift0 v1) t) (at (lift0 v2) t)
    where types = (v1::Int, v2::Int)
 
 
